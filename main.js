5de82387f88e206ed91ce4081aeabba9acea211b
weeklyd3 <timothywei.spam@gmail.com>
2022-05-27 17:29:04 +0000

    Rudimentary system

--- main.js
+++ main.js
@@ -8,10 +8,7 @@ const createWindow = () => {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
     width: 800,
-    height: 600,
-    webPreferences: {
-      preload: path.join(__dirname, 'preload.js')
-    }
+    height: 600
   })
 
   // and load the index.html of the app.
