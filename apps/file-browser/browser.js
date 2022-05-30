86cbc050fa9754f694ffbb38a04caa682323834a
weeklyd3 <timothywei.spam@gmail.com>
2022-05-30 23:18:16 +0000

    Add file browser

--- apps/file-browser/browser.js
+++ apps/file-browser/browser.js
@@ -7,16 +7,20 @@ function getFiles(dir) {
         if (error) {
             return {"status": false, "error": error};
         }
+        files.unshift('..');
         for (var i = 0; i < files.length; i++) {
             const row = document.createElement('tr');
             row.addEventListener('dblclick', function() {
-
+                if (this.children[1].textContent === 'File') return;
+                getFiles(this.children[0].getAttribute('data-goesto'));
             });
             const file = files[i];
             fs.stat(`${dir}/${files[i]}`, function(err, success) {
                 if (err) return false;
                 var name = document.createElement('td');
                 name.textContent = fs.realpathSync(`${dir}/${file}`);
+                name.setAttribute('data-goesto', fs.realpathSync(`${dir}/${file}`));
+                if (file === "..") name.innerHTML = '..';
                 row.appendChild(name);
                 var isFile = document.createElement('td');
                 isFile.textContent = success.isDirectory() ? 'Directory' : 'File';
